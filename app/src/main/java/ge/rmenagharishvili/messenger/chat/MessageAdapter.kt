8e02a3fe3c0b0675ebeb80d9043c24301585c9db
Saba-SH <90729467+Saba-SH@users.noreply.github.com>
2023-07-22 23:28:56 +0400

    show send times for messages

--- app/src/main/java/ge/rmenagharishvili/messenger/chat/MessageAdapter.kt
+++ app/src/main/java/ge/rmenagharishvili/messenger/chat/MessageAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import ge.rmenagharishvili.messenger.databinding.IncomingMessageBinding
 import ge.rmenagharishvili.messenger.databinding.OutgoingMessageBinding
+import ge.rmenagharishvili.messenger.hourAndMinuteFromMillis
 
 class MessageAdapter(
     val context: Context,
@@ -43,10 +44,12 @@ class MessageAdapter(
             // i am stuff
             holder as OutgoingViewHolder
             holder.sentMessage.text = message.messageText
+            holder.time.text = message.timeMillis?.let { hourAndMinuteFromMillis(it) }
         } else {
             // they is stuff
             holder as IncomingViewHolder
             holder.receivedMessage.text = message.messageText
+            holder.time.text = message.timeMillis?.let { hourAndMinuteFromMillis(it) }
         }
     }
 
@@ -57,11 +60,13 @@ class MessageAdapter(
     class OutgoingViewHolder(binding: OutgoingMessageBinding) :
         ViewHolder(binding.root) {
         val sentMessage = binding.tvMessage
+        val time = binding.tvSendTime
     }
 
     class IncomingViewHolder(binding: IncomingMessageBinding) :
         ViewHolder(binding.root) {
         val receivedMessage = binding.tvMessage
+        val time = binding.tvSendTime
     }
 
     companion object {
